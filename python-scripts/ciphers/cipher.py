62c12287f38a7cadd567874852e2ae224ec89594
Quickinline <adelbouhraoua23@gmail.com>
2021-12-04 01:51:42 +0100

    added des cipher to scripts folder

--- python-scripts/ciphers/cipher.py
+++ python-scripts/ciphers/cipher.py
@@ -1,7 +1,6 @@
 
 
 class Cipher:
-
     @classmethod
     def shift(cls,letter:str, shiftValue:int):
         minimum, maximum = Cipher.getBounds(letter)
@@ -20,3 +19,42 @@ class Cipher:
             maximum, minimum = ord('Z'), ord('A')
         
         return minimum, maximum
+    
+    @classmethod
+    def text_to_bits(cls, text:str):
+        binary_result = ''
+        for character in text:
+            
+            # bits= '{0:b}'.format(ord(character))
+            # bits = ''.join(['0' for i in range(8-len(bits))]) + bits
+            # binary_result+=bits
+            binary_result+=bin(ord(character))[2:].zfill(8)
+        
+        return binary_result
+    
+    @classmethod
+    def bits_to_text(cls, bits:str):
+        text = ''
+        for i in range(0,len(bits),8):
+            character = int(bits[i:i+8],2)
+            text+= chr(character)
+        
+        return text
+    
+    @classmethod
+    def _cut_text(cls, bits:str):
+        if len(bits) % 2 != 0:
+            raise Exception('bits passed have odd length, can\'t produce same-length left and right ')
+        middle = int(len(bits)/2)
+        return bits[:middle], bits[middle:]
+    
+    @classmethod
+    def shift_text(cls, text:str, shift_value:int, side='left'):
+        if side == 'left':
+            output = text[shift_value:] + text[:shift_value]
+        elif side == 'right':
+            # TODO: code this
+            raise Exception('Not implemented')
+        return output
+        
+
--- python-scripts/ciphers/des.py
+++ python-scripts/ciphers/des.py
@@ -1,6 +1,136 @@
+from cipher import Cipher
+from matrices import ipMatrix, pc1, pc2, shift_number, expansion_table, listSBox, fpermutation, ip_1
+
+class DesCipher(Cipher):
+
+    @classmethod
+    def _bits_to_blocks(cls, bits:str):
+        blocks = []
+        for i in range(0,len(bits),64):
+            if i+64 < len(bits) -1:
+                block = bits[i:i+64]
+            else:
+                # TODO: add block fill handling
+                block = bits[i:i+64]
+            
+            blocks.append(block)
+    
+    @classmethod
+    def _ip(cls,block:str):
+        if len(block) != 64:
+            raise Exception('passed Block length is not 64bits')
+        return cls._apply_table(block,ipMatrix)
+    
+    # applies table shift to a bit list
+    @classmethod
+    def _apply_table(cls, text:str, table:list, index_starts=1):
+        outbounds = filter(lambda x: x - index_starts > len(text) -1,table)
+        if len(list(outbounds)) > 0:
+            raise Exception('Table applied contains out of bounds values')
+        scribbled = ''
+        for i in range(len(table)):
+            table_index= table[i]
+            scribbled+=text[table_index-index_starts]
+        return scribbled
+    
+    @classmethod
+    def _pc1(cls,key:str):
+        if len(key) != 64:
+            raise Exception('Key length should be 64 to generate 56 bit key')
+        return cls._apply_table(key, pc1, index_starts=0)
 
 
-class DesCipher:
+    @classmethod
+    def _lsi(cls, key:str, round:int):
+        l, r = cls._cut_text(key)
+        l= cls.shift_text(l,shift_number[round])
+        r= cls.shift_text(r,shift_number[round])
+        return l+r
+    
 
-    def initialPermutation():
-        pass
+    @classmethod
+    def compression_permutation(cls, key:str):
+        if len(key) != 64:
+            raise Exception('Key length should be 64 to generate 48 bit keys')
+        key_56 = cls._pc1(key)
+        k = key_56
+        key_rounds = []
+        for i in range(16):
+            k = cls._lsi(k, i)
+            key_rounds.append(k)
+            
+        for i in range(len(key_rounds)):
+            key_rounds[i] = cls._apply_table(key_rounds[i],pc2)
+        
+        return key_rounds
+
+    @classmethod
+    def xor(cls,a:str, b:str):
+        result = ''
+        for i in range(len(a)):
+            result+= '1' if a[i] != b[i] else '0'
+        return result
+    
+
+    @classmethod
+    def _f(cls,halfBlock:str, subKey:str):
+        expandedHalfBlock = cls._apply_table(halfBlock, expansion_table)
+        tempResult = cls.xor(expandedHalfBlock, subKey)
+        # print(tempResult)
+        blocks = [tempResult[i:i+6] for i in range(0,len(tempResult),6)]
+        def apply_sbox(block:str, sbox:list):
+            column = int(block[1:-1],2)
+            line = int(block[0] + block[-1],2)
+
+            return bin(sbox[line][column])[2:].zfill(4)
+        results = ''
+        for i in range(len(blocks)):
+            results += apply_sbox(blocks[i],listSBox[i])
+        
+        return cls._apply_table(results, fpermutation)
+
+
+    @classmethod
+    def encryption_round(cls,l:str, r:str, k:str):
+        return cls.xor(l,cls._f(r,k))
+    
+    @classmethod
+    def encrypt(cls,msg:str, key:str):
+        msg_bits = cls.text_to_bits(msg)[:64] # one block
+        key_bits = cls.text_to_bits(key)[:64] # one block
+        # msg_bits = msg
+        # key_bits = key
+        key_rounds = cls.compression_permutation(key_bits)
+        print(f'Message: {msg_bits}')
+        print(f'IP: {cls._ip(msg_bits)}')
+        l,r = cls._cut_text(cls._ip(msg_bits))
+        
+        for i in range(16):
+            print(f'L{i} = {l} / R{i} = {r}')
+            newl = r
+            r = cls.encryption_round(l,r,key_rounds[i])
+            l = newl
+        cryptedBinary = cls._apply_table(r+l,ip_1)
+        return cls.bits_to_text(cryptedBinary)
+
+
+
+
+# key_bits = DesCipher.text_to_bits('hello world')[:64]
+# print(key_bits)
+# keys_48 = DesCipher.compression_permutation('0001001100110100010101110111100110011011101111001101111111110001')
+
+# e_k1 = DesCipher._f('11110000101010101111000010101010', '000110110000001011101111111111000111000001110010')
+#key = DesCipher.bits_to_text(bits)
+import pprint
+# pprint.pprint(e_k1)
+# print(key)
+
+# m = '0000000100100011010001010110011110001001101010111100110111101111'
+# k = '0001001100110100010101110111100110011011101111001101111111110001'
+
+m = 'hello wo'
+k = 'bonjourj'
+r = DesCipher.encrypt(m,k)
+
+pprint.pprint(r)
